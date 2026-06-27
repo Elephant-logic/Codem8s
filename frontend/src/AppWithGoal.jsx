7a8132f9c5bf4f3a760325858824ec43d933e635
Elephant_logic <keith.davies1978@gmail.com>
2026-06-27 13:37:06 +0100

    Add project bridge to shared shell

--- frontend/src/AppWithGoal.jsx
+++ frontend/src/AppWithGoal.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import GoalPanel from './GoalPanel.jsx';
 import App from './App.jsx';
 import { ProjectProvider } from './projectContext.jsx';
+import ProjectBridge from './projectBridge.jsx';
 
 export default function AppWithGoal() {
   return (
     <ProjectProvider>
+      <ProjectBridge />
       <div className="app">
         <GoalPanel />
       </div>
