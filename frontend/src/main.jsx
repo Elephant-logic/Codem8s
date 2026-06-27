e01b7034753d639cc99b63ed9d2e2c526002ef15
Elephant_logic <keith.davies1978@gmail.com>
2026-06-27 12:08:31 +0100

    Remove separate goal runner mount

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import GoalPanel from './GoalPanel.jsx';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <div className="app">
-      <GoalPanel />
-    </div>
-    <App />
-  </>
-);
+createRoot(document.getElementById('root')).render(<App />);
