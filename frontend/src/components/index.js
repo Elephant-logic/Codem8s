8139e35058a60ceac0052955cb4c5ab952f673d6
Elephant_logic <keith.davies1978@gmail.com>
2026-06-27 14:09:47 +0100

    Add component barrel exports

---
+++ frontend/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as WorkspacePanel } from './WorkspacePanel.jsx';
+export { default as SnapshotPanel } from './SnapshotPanel.jsx';
+export { default as CommandPanel } from './CommandPanel.jsx';
+export { default as LivePreviewPanel } from './LivePreviewPanel.jsx';
+export { default as ProjectBrainPanel } from './ProjectBrainPanel.jsx';
