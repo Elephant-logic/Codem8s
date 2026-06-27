a0fdc3ad9bb0251b4709043309e11f2414af3bc6
Elephant_logic <keith.davies1978@gmail.com>
2026-06-27 10:10:02 +0100

    Wire goal runner into app shell

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import GoalPanel from './GoalPanel.jsx';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <div className="app">
+      <GoalPanel />
+    </div>
+    <App />
+  </>
+);
